ea7616a26d6e3469a4c4515989660eb8d1ced00d
Eric S <esimonds09@gmail.com>
2023-09-12 02:51:31 -0400

    Update index.vue file for homepage. Add Conversation page.

---
+++ frontend/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-btn :class="btnClass" @click="handleClick">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    primary: Boolean,
+    secondary: Boolean,
+    disabled: Boolean,
+  },
+  computed: {
+    btnClass() {
+      return {
+        "v-btn--contained": this.primary,
+        "v-btn--outlined": this.secondary,
+        "v-btn--disabled": this.disabled,
+      };
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
---
+++ frontend/pages/Conversation.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container class="text-center center align">
+    <h2>Let's Talk</h2>
+    <p>Here's where the conversation happens!</p>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
